199ecf1f4658b2ef039a74133ea8f9c0441d628f
Erik C. Thauvin <erik@thauvin.net>
2017-04-17 23:01:49 -0700

    IDEA and Kobalt updates.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,11 +1,10 @@
 
-import com.beust.kobalt.plugin.application.application
 import com.beust.kobalt.plugin.packaging.assemble
 import com.beust.kobalt.profile
 import com.beust.kobalt.project
-//import net.thauvin.erik.kobalt.plugin.propertyfile.*
 
 val semver = "0.1.0"
+
 
 val dev by profile()
 val kobaltDependency = if (dev) "kobalt" else "kobalt-plugin-api"
@@ -31,27 +30,4 @@ val p = project {
 
         mavenJars {}
     }
-
-    application {
-        mainClass = "com.example.MainKt"
-    }
-}
-
-val example = project(p) {
-    directory = "example"
-    name = "example"
-    version = "0.1.0"
-
-    assemble {
-        jar {
-        }
-    }
-
-    application {
-        mainClass = "com.example.MainKt"
-    }
-
-//    propertyFile {
-//        file = "version.properties"
-//    }
 }
